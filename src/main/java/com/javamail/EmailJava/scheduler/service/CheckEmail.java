91e7c1074de12a5c6ec64574057da5ab1a644622
MATEUS COSTA DOS SANTOS <mateus.csantos-ext@viavarejo.com.br>
2019-06-13 16:39:49 -0300

    Retirada imports

--- src/main/java/com/javamail/EmailJava/scheduler/service/CheckEmail.java
+++ src/main/java/com/javamail/EmailJava/scheduler/service/CheckEmail.java
@@ -6,7 +6,6 @@ import javax.mail.Address;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Store;
